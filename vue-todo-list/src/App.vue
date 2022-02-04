beb88351df2f74fd33d5a9a160cbf56aae8478ab
Takahiro Abe <hirotaka081630@gmail.com>
2022-02-05 06:57:16 +0900

    feat(p.50-1): todoの表示

--- vue-todo-list/src/App.vue
+++ vue-todo-list/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <!-- ToDoListを使用する -->
+  <ToDoList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ToDoList from './components/ToDoList.vue';
 export default {
   name: 'App',
+  // ToDoListを<template>で使用できるようにする
   components: {
-    HelloWorld
-  }
-}
+    ToDoList,
+  },
+};
 </script>
---
+++ vue-todo-list/src/components/ToDoList.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul>
+    <li>{{ todo }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return { todo: 'Go out to sea' };
+  },
+};
+</script>
