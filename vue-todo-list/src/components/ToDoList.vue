605d87b2f82ebbe621bba9aea1e699607b96acf2
Takahiro Abe <hirotaka081630@gmail.com>
2022-02-05 11:07:25 +0900

    feat(p54-1): todoリストのフィルタ機能を実装
    computed: オプション

--- vue-todo-list/src/components/ToDoList.vue
+++ vue-todo-list/src/components/ToDoList.vue
@@ -2,8 +2,9 @@
   <div>
     <input v-model="inputValue" />
     <button v-on:click="handleClick">TODOを追加</button>
+    <input v-model="filterValue" placeholder="フィルタテキスト" />
     <ul>
-      <li v-for="todo in todoItems" v-bind:key="todo.id" v-on:click="todo.done = !todo.done">
+      <li v-for="todo in filterdTodoItems" v-bind:key="todo.id" v-on:click="todo.done = !todo.done">
         <span v-if="todo.done">✅</span>
         {{ todo.text }}
       </li>
@@ -20,7 +21,18 @@ export default {
         { id: 1, done: false, text: 'Go out to sea' },
         { id: 2, done: true, text: 'Invite the first member' },
       ],
+      filterValue: '',
     };
+  },
+  computed: {
+    filterdTodoItems() {
+      if (!this.filterValue) {
+        return this.todoItems;
+      }
+      return this.todoItems.filter((todo) => {
+        return todo.text.includes(this.filterValue);
+      });
+    },
   },
   methods: {
     handleClick() {
